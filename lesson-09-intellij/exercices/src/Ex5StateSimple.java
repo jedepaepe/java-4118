0de3d21a31658d947c595dcd7cb72d00f7d3053e
jedepaepe <jedepaepe@epfc.eu>
2021-11-07 19:20:20 +0100

    exercice state simple

---
+++ lesson-09-intellij/exercices/src/Ex5StateSimple.java
@@ -0,0 +1,39 @@
+import processing.core.PApplet;
+
+public class Ex5StateSimple extends PApplet {
+    int step = 0;
+
+    @Override
+    public void settings() {
+        size(300, 300);
+    }
+
+    @Override
+    public void draw() {
+        switch (step) {
+            case 0:
+                background(255, 0, 0);
+                break;
+            case 1:
+                background(0, 255, 0);
+                break;
+            case 2:
+                background(0, 0, 255);
+                break;
+        }
+    }
+
+    @Override
+    public void mousePressed() {
+        step = (step + 1) % 3;
+    }
+
+    @Override
+    public void keyPressed() {
+        step = (step + 1) % 3;
+    }
+
+    public static void main(String[] args) {
+        PApplet.runSketch(new String[]{ "Ex5StateSimple "}, new Ex5StateSimple());
+    }
+}
