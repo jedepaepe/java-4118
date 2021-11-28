db907b0ba4595a13a1d2fa62fbe696cb8ed3fede
Jean-Michel Depaepe <jedepaepe@epfc.eu>
2021-11-28 11:57:38 +0100

    object approach

--- lesson-09-intellij/exercices/src/Ex8Revision.java
+++ lesson-09-intellij/exercices/src/Ex8Revision.java
@@ -1,14 +1,14 @@
 import processing.core.PApplet;
 
+import java.util.ArrayList;
+
 public class Ex8Revision extends PApplet {
     final int SQUARE_COUNT = 6;
     final int SQUARE_SIZE = 100;
     final int CIRCLE_SIZE_DELTA = 10;
     final int CIRCLE_MOVE = 5;
-    int circleSize = 30;
-    int[] positionX = new int[1000];
-    int[] positionY = new int[1000];
-    int index = 0;
+    int diameter = 30;
+    ArrayList<Circle> circles = new ArrayList<>();
 
     @Override
     public void settings() {
@@ -24,8 +24,13 @@ public class Ex8Revision extends PApplet {
     @Override
     public void draw() {
         background(0);
+        drawSquares();
+        drawCircles();
+    }
+
+    private void drawSquares() {
         for (int i = 0; i < SQUARE_COUNT; ++i) {
-            int p = i * SQUARE_SIZE;
+            int p = pixelsFromIndex(i);
             if (i % 2 == 0) fill(255, 255, 0);
             else fill(0, 255, 255);
             square(p, p, SQUARE_SIZE);
@@ -33,51 +38,77 @@ public class Ex8Revision extends PApplet {
             else fill(255, 255, 0);
             text("carré " + (i + 1), p + SQUARE_SIZE / 2, p + SQUARE_SIZE /2);
         }
+    }
 
-        for (int i = 0; i < index; ++i) {
-            fill(255, 0, 255);
-            circle(positionX[i], positionY[i], circleSize);
-        }
+    private int pixelsFromIndex(int index) {
+        return index * SQUARE_SIZE;
+    }
+
+    private void drawCircles() {
+        fill(255, 0, 255);
+        for (Circle c : circles) circle(getInWindowPixels(c.x, width), getInWindowPixels(c.y, height), diameter);
     }
 
     @Override
     public void mouseClicked() {
-        positionX[index] = mouseX;
-        positionY[index] = mouseY;
-        ++index;
+        addCircle();
+    }
+
+    private void addCircle() {
+        circles.add(new Circle(mouseX, mouseY));
     }
 
     @Override
     public void keyPressed() {
-        if ('+' == key && circleSize < 100) circleSize += CIRCLE_SIZE_DELTA;
-        if ('-' == key && circleSize > 10) circleSize -= CIRCLE_SIZE_DELTA;
+        if ('+' == key && diameter < 100) diameter += CIRCLE_SIZE_DELTA;
+        if ('-' == key && diameter > 10) diameter -= CIRCLE_SIZE_DELTA;
 
-        int deltaX = 0;
-        int deltaY = 0;
         switch (keyCode) {
             case UP:
-                deltaY = - CIRCLE_MOVE;
+                moveCircles(0, -CIRCLE_MOVE);
                 break;
             case RIGHT:
-                deltaX = CIRCLE_MOVE;
+                moveCircles(CIRCLE_MOVE, 0);
                 break;
             case DOWN:
-                deltaY = CIRCLE_MOVE;
+                moveCircles(0, CIRCLE_MOVE);
                 break;
             case LEFT:
-                deltaX = - CIRCLE_MOVE;
+                moveCircles(- CIRCLE_MOVE, 0);
         }
-        if (0 != deltaX || 0 != deltaY) {
-            for (int i = 0; i < index; ++i) {
-                positionX[i] = (positionX[i] + deltaX) % width;
-                if (positionX[i] < 0) positionX[i] = positionX[i] + width;
-                positionY[i] = (positionY[i] + deltaY) % height;
-                if (positionY[i] < 0) positionY[i] = positionY[i] + height;
-            }
+    }
+
+    private void moveCircles(int deltaX, int deltaY) {
+        for (Circle c : circles) {
+            c.move(deltaX, deltaY);
         }
+    }
+
+    private int getInWindowPixels(int position, int size) {
+        int result = position % size;
+        if (result < 0) result += size;
+        return result;
     }
 
     public static void main(String[] args) {
         PApplet.runSketch(new String[] { Ex8Revision.class.getName() }, new Ex8Revision());
+    }
+
+    class Circle {
+        int x;
+        int y;
+
+        public Circle() {
+        }
+
+        public Circle(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void move(int deltaX, int deltaY) {
+            x += deltaX;
+            y += deltaY;
+        }
     }
 }
