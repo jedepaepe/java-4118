8b1cfd2fb16213902fc6e1d96eebc35feacb8678
Jean-Michel Depaepe <jedepaepe@epfc.eu>
2021-11-28 08:46:19 +0100

    step 1 board

--- projet-snake/src/SnakeApp.java
+++ projet-snake/src/SnakeApp.java
@@ -1,6 +1,33 @@
 import processing.core.PApplet;
 
 public class SnakeApp extends PApplet {
+    int size = 800;
+    int numberOfCells = 10;
+
+    @Override
+    public void settings() {
+        size(size, size);
+    }
+
+    @Override
+    public void setup() {
+        surface.setTitle("Snake");
+    }
+
+    @Override
+    public void draw() {
+        background(0);
+        drawBoard();
+    }
+
+    private void drawBoard() {
+        stroke(255);
+        for (int xy = 0; xy < size; xy += size / numberOfCells) {
+            line(xy, 0, xy, height);
+            line(0, xy, width, xy);
+        }
+    }
+
     public static void main(String[] args) {
         PApplet.runSketch(new String[]{"SnakeApp"}, new SnakeApp());
     }
